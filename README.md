86668538e12a6f3629bb417aac6477418ac34c63
Kenny Speer <kenny.speer@gmail.com>
2018-05-29 13:40:36 -0700

    Initial commit of script and instructions

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# kernel
-My Linux Kernel 
+# ubuntu / debian kernel
+Build script for my kernel that includes aufs patches, output is an installed kernel.
+
+This script will create a local copy of your source dir (so you don't have reverse patches) and then build there ... this will keep your source clean (yes, you can git checkout . etc etc but why?)
+
+This is my daily script that I use to build in about 30min (Dell Precision 5520 Laptop, i7-7820HQ, 32GB Mem, 1TB NVMe)
+
+# Instructions
+
+**Step 1:** Clone AUFS repository as a subdir in this repository
+This script adds AUFS functionality for those of us with docker images on aufs.
+
+```
+git clone git@github.com:sfjro/aufs-standalone
+cd aufs-standalone
+git checkout aufs4.x-rcN
+cd ..
+```
+
+**Step 2:** Clone linux kernel repo
+Here is a choice to clone either the linux stable tree or Linus stable tree ... Linus tree will be more current but may miss a patch or two and may be less stable.
+To clone linux-stable:
+```
+./kern.sh clone
+```
+
+To clone Linus tree:
+```
+./kern.sh clone linus
+```
+
+**Step 3:** Update your source and list the tags
+```
+./kern.sh pull
+```
+
+Filter the list of tags via grep:
+```
+./kern.sh pull |grep v4.17
+```
+
+**Step 4:** Finally, build and install your kernel
+```
+./kern.sh build v4.17-rc7
+```
---
+++ kern.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+
+dprint() {
+    echo "---> ${@}" 2>&1
+}
+
+if [ "$1" == "clone" ]; then
+
+    # clone linux source to new dir
+    dprint "cloning kernel/git/stable/linux-stable"
+    rm -rf linux.stable
+
+    if [ "x${2}" == "xlinus" ]; then
+        # linus tree
+        git clone git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git linux.stable
+    else
+        git clone git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git linux.stable
+    fi
+    exit 0
+
+elif [ "$1" == "pull" ]; then
+    
+    dprint "updating source tree"
+    # update linux source from git
+    cd linux.stable
+    git pull
+    git tag 
+
+    exit 0
+
+elif [ "$1" == "build" ]; then
+
+    # create new copy of tree, patch it and build it
+    BRANCH=$2
+    rm -rf linux build
+    rsync -a --info=progress2 linux.stable/* linux/ 
+    cd linux
+    
+    if [ "x${BRANCH}" == "x" ]; then
+        dprint "building master since no branch was provided"
+        BRANCH='master'
+    else
+        dprint "building ${BRANCH}"
+    fi
+
+    git checkout ${BRANCH}
+
+    dprint "patching from aufs4"
+    patch -p1 --ignore-whitespace -i ../aufs4-standalone/aufs4-kbuild.patch
+    patch -p1 --ignore-whitespace -i ../aufs4-standalone/aufs4-base.patch
+    patch -p1 --ignore-whitespace -i ../aufs4-standalone/aufs4-mmap.patch
+    patch -p1 --ignore-whitespace -i ../aufs4-standalone/aufs4-standalone.patch
+
+    cp -R ../aufs4-standalone/Documentation .
+    cp -R ../aufs4-standalone/fs .
+    cp -R ../aufs4-standalone/include .
+
+    dprint "patched kernel source and copied aufs src files"
+
+else
+    dprint "must provide clone, build, or pull as parameter"
+    exit 1
+fi
+
+# configure it
+yes '' |make oldconfig
+
+# enable AUFS from the patches above
+sed -i 's/# CONFIG_AUFS_FS is not set/CONFIG_AUFS_FS=y/g' .config
+
+# finally, build it
+make -j `getconf _NPROCESSORS_ONLN` deb-pkg LOCALVERSION=-reeps
+
+# move build output
+cd ..
+rm -rf build
+mkdir build
+mv linux-* build/
+
+rm build/*-dbg_*.deb
+sudo dpkg -i build/*.deb
